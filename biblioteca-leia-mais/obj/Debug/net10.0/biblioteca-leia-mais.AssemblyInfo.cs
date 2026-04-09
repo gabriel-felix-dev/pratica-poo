696715db9b59e3ccc5735ac46b40ce64875accdc
Gabriel <gabrielfelix099@hotmail.com>
2026-04-09 12:03:42 -0300

    feat/biblioteca-leia-mais: Atualizações automáticas do projeto

--- biblioteca-leia-mais/obj/Debug/net10.0/biblioteca-leia-mais.AssemblyInfo.cs
+++ biblioteca-leia-mais/obj/Debug/net10.0/biblioteca-leia-mais.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("biblioteca-leia-mais")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c314dc26f0d6b697c52eddd9b5b4316427f2c25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3560f60eb35dfdcc0b119e97b47f33ef128eba8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("biblioteca-leia-mais")]
 [assembly: System.Reflection.AssemblyTitleAttribute("biblioteca-leia-mais")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
